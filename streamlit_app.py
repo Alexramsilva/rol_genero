83ba79f049776a45865f627554f057e13f7181d6
URC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2024-10-22 21:28:42 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
